a7550f618e548ae8cafd179420b5372dbf07d88b
bjornus <dmitro.szewczuk@wp.pl>
2017-06-12 10:28:53 +0200

    vec2.hpp documentated

--- src/include/Cerium/vec2.hpp
+++ src/include/Cerium/vec2.hpp
@@ -8,9 +8,27 @@ namespace cerium
     struct vec2
     {
         float x, y;
-        vec2(void);
-        vec2(const float & value);
-        vec2(const float & valueX, const float & valueY);
+        /*
+            ^[start]
+            ^[class][vec2]
+            ^[description][Setting 0 as value of attributes of vector]
+        */
+        vec2(void); //^header
+        /*
+            ^[start]
+            ^[class][vec2]
+            ^[description][Setting given argument as value of attributes of vector]
+            ^[argument]<value>[Value of vector]
+        */
+        vec2(const float & value); //^header
+        /*
+            ^[start]
+            ^[class][vec2]
+            ^[description][Setting 0 as value of attributes of vector]
+            ^[argument]<valueX>[Value of x attribute of vector]
+            ^[argument]<valueY>[Value of x attribute of vector]
+        */
+        vec2(const float & valueX, const float & valueY); //^header
 
         friend std::ostream & operator<<(std::ostream & o, const vec2 & v);
     };
