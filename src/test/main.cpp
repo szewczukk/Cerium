54b23c065fd752464fef7e53db20b906610719b9
bjornus <dmitro.szewczuk@wp.pl>
2017-06-13 20:25:55 +0200

    Getting size of window from XML file

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -2,11 +2,27 @@
 #include "../include/Cerium/Window.hpp"
 #include "../include/Cerium/EventManager.hpp"
 
+#include <rapidxml.hpp>
+#include <rapidxml_utils.hpp>
+
 #include <iostream>
+
+
+cerium::vec2 size_of_window()
+{
+    rapidxml::file <> file("settings.xml");
+    rapidxml::xml_document<> settings;
+    settings.parse<0>(file.data());
+    rapidxml::xml_node <> * size = settings.first_node("settings")->first_node("size");
+
+    int width = atoi(size->first_attribute("width")->value());
+    int height = atoi(size->first_attribute("height")->value());
+    return {width, height};
+}
 
 int main()
 {
-    cerium::Window::setSize({1024, 768});
+    cerium::Window::setSize(size_of_window());
     cerium::Window::setTitle("Cerium");
 
     cerium::Window::init();
