792b5ceb5a39454f274ca61ae6e4b4512b3b667a
bjornus <dmitro.szewczuk@wp.pl>
2017-06-26 23:16:23 +0200

    Children mechanics added to Prop

--- src/include/Cerium/Prop.hpp
+++ src/include/Cerium/Prop.hpp
@@ -1,6 +1,8 @@
 #ifndef CERIUM_PROP_HPP
 #define CERIUM_PROP_HPP
 
+#include <map>
+
 namespace cerium
 {
     class Person;
@@ -17,20 +19,32 @@ namespace cerium
         {
             this->basePerson = basePerson;
             this->name = name;
-        };
+        }
         /*
             ^[name][update]
             ^[class][Prop]
             ^[description][Updating Prop with deltaTime]
             ^[argument]<deltaTime>[DeltaTime of main game clock]
          */
-        virtual void update(const float & deltaTime) = 0; //^header
+        virtual void update(const float & deltaTime) //^header
+        {
+            for(auto & child : children)
+            {
+                child.second->update(deltaTime);
+            }
+        }
         /*
             ^[name][draw]
             ^[class][Prop]
             ^[description][Abstract class to draw the object into screen]
          */
-        virtual void draw(void) = 0; //^header
+        virtual void draw(void) //^header
+        {
+            for(auto & child : children)
+            {
+                child.second->draw();
+            }
+        }
         /*
             ^[name][getName]
             ^[class][Prop]
@@ -49,9 +63,52 @@ namespace cerium
         {
             return *basePerson;
         }
+        /*
+            ^[name][exist]
+            ^[class][Prop]
+            ^[description][Check existing of Prop with given name]
+            ^[argument]<name>[Name of Prop to check existing]
+            ^[returns][True when the Prop with given name exist or False when it isn't]
+         */
+        bool exist(const std::string & name) //^header
+        {
+            for(auto & child : children)
+            {
+                if(child.first == name) return true;
+            }
+            return false;
+        }
+        /*
+            ^[name][addChild]
+            ^[class][Prop]
+            ^[description][Adding new prop as child]
+            ^[argument]<prop>[Pointer to Prop to add]
+         */
+        void addChild(Prop * prop) //^header
+        {
+            if(!exist(prop->getName()))
+            {
+                children[prop->getName()] = prop;
+            }
+        }
+        /*
+            ^[name][getChildren]
+            ^[class][Prop]
+            ^[argument]<name>[Name of Prop to return]
+            ^[returns][Pointer to children with given name]
+         */
+        Prop * getChildren(const std::string & name) //^header
+        {
+            for(auto & child : children)
+            {
+                if(child.first == name) return child.second;
+            }
+            return nullptr;
+        }
     protected:
         Person * basePerson;
         std::string name;
+        std::map<std::string, Prop*> children;
     };
 }
 
