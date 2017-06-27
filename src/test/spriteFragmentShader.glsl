7ccfa0e3aa25ac3bd2622657f88518c9be4870f6
bjornus <dmitro.szewczuk@wp.pl>
2017-06-27 23:00:06 +0200

    Advanced Fragment shader

--- src/test/spriteFragmentShader.glsl
+++ src/test/spriteFragmentShader.glsl
@@ -4,9 +4,19 @@ in vec2 textureCoordinates;
 
 out vec4 color;
 
-uniform sampler2D sampler;
+uniform sampler2D textureSampler;
+
+uniform vec4 uColor;
+uniform bool textured;
 
 void main()
 {
-    color = texture(sampler, textureCoordinates);
+    if (textured)
+    {
+        color = texture(textureSampler, textureCoordinates);
+    }
+    else
+    {
+        color = uColor;
+    }
 }
