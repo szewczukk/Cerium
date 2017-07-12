ccac4b04c2473e67d961a3189c73d8325101e58f
bjornus <dmitro.szewczuk@wp.pl>
2017-07-12 12:24:42 +0200

    player .lua changed, other Person changed

--- core/test/res/scripts/player.lua
+++ core/test/res/scripts/player.lua
@@ -13,6 +13,8 @@ function update(deltaTime)
     elseif inputManager.isKeyPressed(KEY_LEFT) then
         move(vec2.new(-speed * deltaTime, 0))
     elseif inputManager.isKeyPressed(KEY_UP) then
-        move(vec2.new(0, -speed * deltaTime * 10))
+        move(vec2.new(0, -speed * deltaTime))
+    elseif inputManager.isKeyPressed(KEY_DOWN) then
+        move(vec2.new(0, speed * deltaTime))
     end
 end
