780d9765a623b2dc71dbdb326c9b5b1cdf9dbef1
bjornus <dmitro.szewczuk@gmail.com>
2017-08-24 22:26:50 +0200

    editor updated

--- editor/editor.py
+++ editor/editor.py
@@ -1,20 +1,26 @@
-name = ""
+project_name = ""
 while True:
-    action = input("{project} > ".format(project=name))
+    action = input("{project_name} > ".format(project_name=project_name))
 
-    if action == "init":
+    if action == "init" or action == "i":
         name = input("  > name=")
+        path = input("  > path=")
 
-    elif action == "add":
-        if name != "":
+    elif action == "add" or action == "a":
+        if project_name != "":
             obj = input("  > object=")
             if obj == "resource" or obj == "res":
                 print("res")
             elif obj == "scene" or obj == "scn":
                 print("scn")
+        else:
+            print("  > There is no set project")
+    elif action == "remove" or action == "r":
+        if project_name != "":
+            obj = input("  > object=")
 
-    elif action == "set":
-        name = input("  > name=")
+    elif action == "set" or action == "s":
+        project_name = input("  > name=")
 
-    elif action == "exit":
+    elif action == "exit" or action == "e":
         break
