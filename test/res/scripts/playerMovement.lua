301e8d22a1414e2a2608b9a6fe0c410084fee343
bjornus <dmitro.szewczuk@gmail.com>
2017-09-10 14:05:49 +0200

    playerMovement.lua patched

--- test/res/scripts/playerMovement.lua
+++ test/res/scripts/playerMovement.lua
@@ -1,16 +1,28 @@
+velocity = nil
+acceleration = nil
+
 -- Function running once, at start of game
 function init()
-
+	velocity = vec2:new(0)
+	acceleration = vec2:new(0)
 end
 
 -- Function running non-stop
 function update(deltaTime)
 	if eventManager.isKeyPressed(KEY_LEFT) then
-		this.move(vec2:new(-300000 * deltaTime, 0))
+		velocity.x = -300000 * deltaTime
 	elseif eventManager.isKeyPressed(KEY_RIGHT) then
-		this.move(vec2:new( 300000 * deltaTime, 0))
+		velocity.x = 300000 * deltaTime
+	else
+		velocity.x = 0
 	end
+	
 	if eventManager.isKeyPressed(KEY_UP) and cast_to_rigidbody(this.getProp("rigidbody")):isOnGround() then
-		cast_to_rigidbody(this.getProp("rigidbody")):applyForce(vec2:new(0, -300000))
+		acceleration.y = -300000
+	else
+		acceleration.y = 0
 	end
+	
+	this.move(velocity)
+	cast_to_rigidbody(this.getProp("rigidbody")):applyForce(acceleration)
 end
